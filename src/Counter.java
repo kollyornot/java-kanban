d8445f6dfd9d7d4a8b69fbe93e5a9811d669db99
Maria Zvonchuk <kollyornot@gmail.com>
2025-01-10 13:53:05 +0200

    fixed some more mistakes

---
+++ src/Counter.java
@@ -0,0 +1,7 @@
+public class Counter {
+    static int counter = 0;
+
+    public int getId(){
+        return ++counter;
+    }
+}
--- src/TaskManager.java
+++ src/TaskManager.java
@@ -5,7 +5,8 @@ public class TaskManager {
     private HashMap<Integer, Task> tasks;
     private HashMap<Integer, Epic> epics;
     private HashMap<Integer, SubTask> subTasks;
-    int counter = this.hashCode();
+    Counter counter = new Counter();
+    //я не поняла, о чем вы, и сделала так
 
     public ArrayList<Task> taskList() {
         return new ArrayList<>(tasks.values());
@@ -70,54 +71,28 @@ public class TaskManager {
     }
 
     public Task addNewTask(String name, String description, Status status) {
-        Task newTask = new Task(name, description, status, ++counter);
+        Task newTask = new Task(name, description, status, counter.getId());
         tasks.put(newTask.getId(), newTask);
         return newTask;
     }
 
     public Epic addNewEpic(String name, String description, ArrayList<Integer> subTasks) {
-        boolean areAllSubTasksDone = checkAllSubTasksCompleted(subTasks);
-        boolean areAllSubTasksNew = checkAllSubTasksNew(subTasks);
+        boolean areAllSubTasksDone = checkAllSubTasksSameStatus(subTasks, Status.DONE);
+        boolean areAllSubTasksNew = checkAllSubTasksSameStatus(subTasks, Status.NEW);
         if (subTasks.isEmpty() || areAllSubTasksNew) {
-            Epic newEpic = new Epic(name, description, Status.NEW, ++counter, subTasks);
+            Epic newEpic = new Epic(name, description, Status.NEW, counter.getId(), subTasks);
         } else if (areAllSubTasksDone) {
-            Epic newEpic = new Epic(name, description, Status.DONE, ++counter, subTasks);
+            Epic newEpic = new Epic(name, description, Status.DONE, counter.getId(), subTasks);
         }
-        Epic newEpic = new Epic(name, description, Status.IN_PROGRESS, ++counter, subTasks);
+        Epic newEpic = new Epic(name, description, Status.IN_PROGRESS, counter.getId(), subTasks);
         epics.put(newEpic.getId(), newEpic);
         return newEpic;
     }
 
     public SubTask addNewSubTask(String name, String description, Status status, int epicId) {
-        SubTask newSubTask = new SubTask(name, description, status, ++counter, epicId);
+        SubTask newSubTask = new SubTask(name, description, status, counter.getId(), epicId);
         subTasks.put(newSubTask.getId(), newSubTask);
         return newSubTask;
-    }
-
-    public boolean checkAllSubTasksNew(ArrayList<Integer> subTasks) {
-        boolean allSubTasksNew = false;
-        for (Integer subTaskid : subTasks) {
-            if (getSubTaskById(subTaskid).getStatus().equals(Status.NEW))
-                allSubTasksNew = true;
-            else {
-                allSubTasksNew = false;
-                break;
-            }
-        }
-        return allSubTasksNew;
-    }
-
-    public boolean checkAllSubTasksCompleted(ArrayList<Integer> subTasks) {
-        boolean allSubTasksCompleted = false;
-        for (Integer subTaskid : subTasks) {
-            if (getSubTaskById(subTaskid).getStatus().equals(Status.DONE))
-                allSubTasksCompleted = true;
-            else {
-                allSubTasksCompleted = false;
-                break;
-            }
-        }
-        return allSubTasksCompleted;
     }
 
     public Task updateTask(Task task, Status status) {
@@ -137,7 +112,7 @@ public class TaskManager {
 
     public SubTask updateTask(SubTask subTask, Status status) {
         subTask.setStatus(status);
-        checkAllSubTasksCompletedByEpicId(subTask.getEpicId());
+        checkAllSubTasksSameStatusById(subTask.getEpicId(), Status.DONE);
         return subTask;
     }
 
@@ -154,25 +129,39 @@ public class TaskManager {
         subTasks.remove(id);
     }
 
-    public void checkAllSubTasksCompletedByEpicId(int id) {
-        boolean allSubTasksCompleted = false;
+    public boolean checkAllSubTasksSameStatusById(int id, Status status) {
+        boolean allSubTasksSameStatus = false;
         for (Integer subTaskid : getEpicById(id).getSubTasks()) {
-            if (getSubTaskById(subTaskid).getStatus().equals(Status.DONE))
-                allSubTasksCompleted = true;
+            if (getSubTaskById(subTaskid).getStatus().equals(status))
+                allSubTasksSameStatus = true;
             else {
-                allSubTasksCompleted = false;
+                allSubTasksSameStatus = false;
                 break;
             }
         }
-        getEpicById(id).setAreAllSubTasksCompleted(allSubTasksCompleted);
-        if (allSubTasksCompleted)
-            getEpicById(id).setStatus(Status.DONE);
+        if(status.equals(Status.DONE) && allSubTasksSameStatus){
+            getEpicById(id).setAreAllSubTasksCompleted(true);
+            getEpicById(id).setStatus(Status.DONE);}
+        return allSubTasksSameStatus;
     }
 
-    public void AddSubTask(SubTask subTask, Epic epic) {
+    public boolean checkAllSubTasksSameStatus(ArrayList<Integer> subtasksIds, Status status) {
+        boolean allSubTasksSameStatus = false;
+        for (Integer subTaskid : subtasksIds) {
+            if (getSubTaskById(subTaskid).getStatus().equals(status))
+                allSubTasksSameStatus = true;
+            else {
+                allSubTasksSameStatus = false;
+                break;
+            }
+        }
+        return allSubTasksSameStatus;
+    }
+
+    public void addSubTask(SubTask subTask, Epic epic) {
         epic.getSubTasks().add(subTask.getId());
         subTask.setEpicId(epic.getId());
-        checkAllSubTasksCompletedByEpicId(epic.getId());
+        checkAllSubTasksSameStatusById(epic.getId(), Status.DONE);
     }
 
     public void RemoveAllSubTask(Epic epic) {
